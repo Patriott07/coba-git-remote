464475242acf36f8cb755ea5f18a6777364e9886
Patriott07 <pwangtampn@gmail.com>
2023-07-08 11:20:05 +0700

    Menambhkn index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Heloo World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eaque, voluptas porro dolorum iure quo distinctio harum repellendus, impedit provident consequuntur sit blanditiis. Maiores, asperiores reprehenderit vero omnis inventore, illo, consectetur commodi repellat eligendi ex consequuntur ut necessitatibus architecto! Exercitationem obcaecati officiis, deserunt minima, fuga magnam veniam doloremque enim voluptatem itaque corporis incidunt veritatis temporibus odit sit in illo esse. Minus, repellat nostrum neque, totam aliquam dolore quae architecto earum veritatis commodi velit unde porro soluta asperiores, labore maxime! Necessitatibus soluta vel nostrum id error cupiditate nihil perferendis, commodi porro, facilis, dolores fuga rem voluptas labore animi culpa architecto fugiat?</p>
+</body>
+</html>
