c291678d518b47a88debdc8dca794a385740c629
Patriott07 <pwangtampn@gmail.com>
2023-07-08 11:25:34 +0700

    Menambhkn list belajr

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Heloo World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eaque, voluptas porro dolorum iure quo distinctio harum repellendus, impedit provident consequuntur sit blanditiis. Maiores, asperiores reprehenderit vero omnis inventore, illo, consectetur commodi repellat eligendi ex consequuntur ut necessitatibus architecto! Exercitationem obcaecati officiis, deserunt minima, fuga magnam veniam doloremque enim voluptatem itaque corporis incidunt veritatis temporibus odit sit in illo esse. Minus, repellat nostrum neque, totam aliquam dolore quae architecto earum veritatis commodi velit unde porro soluta asperiores, labore maxime! Necessitatibus soluta vel nostrum id error cupiditate nihil perferendis, commodi porro, facilis, dolores fuga rem voluptas labore animi culpa architecto fugiat?</p>
+    <p>Rencana Alur Belajar : </p>
+    <ul>
+        <li>React</li>
+        <li>API</li>
+    </ul>
 </body>
 </html>
